84ff725920fe9e4183626b2e86013db1cf499d13
George Prichici <4246227+georgeprichici@users.noreply.github.com>
2020-08-06 16:27:31 -0700

    Initialize metaDefenderAPI attribute with a new instance using the MetaDefenderAPI factory. Also, the Authorization header is ignored, however the prepare function could be used to allow overriding the apikey to use the one provided by Menlo (this might be useful when you don't want to share the same apikey).

--- metadefender_menlo/api/handlers/base_handler.py
+++ metadefender_menlo/api/handlers/base_handler.py
@@ -1,7 +1,18 @@
 from tornado.web import RequestHandler
+from metadefender_menlo.api.metadefender.metadefender_api import MetaDefenderAPI
 import json
 
 class BaseHandler(RequestHandler):
+
+    def initialize(self):
+        self.metaDefenderAPI = MetaDefenderAPI.get_instance()
+
+    # def prepare(self):
+    #     authorization_token = self.request.headers.get('Authorization')
+        
+    #     if authorization_token:
+    #         self.metaDefenderAPI.apikey = authorization_token
+
     def json_response(self, data, status_code=200):
         print("{0} response: {1}".format(status_code, data))
         self.set_status(status_code)
